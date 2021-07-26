86fb12176b393db176f75d8c289399b4ee582932
zzx <xqr1234568@gmail.com>
2021-07-26 11:23:59 +0800

    semaphore test one

---
+++ src/main/java/SemaphorePractice/SemaphoreTest1/Service.java
@@ -0,0 +1,36 @@
+package SemaphorePractice.SemaphoreTest1;
+
+import java.util.concurrent.Semaphore;
+
+public class Service {
+    private Semaphore semaphore = new Semaphore(1);
+
+    public void test() {
+        try {
+            semaphore.acquire();
+            System.out.println(Thread.currentThread().getName());
+            Thread.sleep(5000);
+            semaphore.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Service service = new Service();
+        Runnable runnable = service::test;
+        Thread threadA = new Thread(runnable);
+        Thread threadB = new Thread(runnable);
+        Thread threadC = new Thread(runnable);
+
+        threadA.setName("A");
+        threadB.setName("B");
+        threadC.setName("C");
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+
+    }
+}
